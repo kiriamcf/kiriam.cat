5ca4c92a1e65685adf465a36714dde525571c46e
kiriamcf <kiriamcf@gmail.com>
2024-08-18 20:16:22 +0200

    smooth scrolling and scroll based text animation on aboutMe

--- components/sections/AboutMe.vue
+++ components/sections/AboutMe.vue
@@ -2,7 +2,10 @@
 import portfolio from '../../public/portfolio.jpg'
 import ContentTitle from '../Text/ContentTitle.vue'
 import Highlight from '../Text/Highlight.vue'
-import { computed } from 'vue'
+import { computed, onMounted } from 'vue'
+import SplitType from 'split-type'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const birthDate = new Date(2001, 11, 21); // Note: December is month 11 (0-indexed)
 
@@ -15,18 +18,48 @@ const age = computed(() => {
     return hasBirthdayPassed ? age : age - 1;
 });
 
+onMounted(() => {
+    if (window.innerWidth < 640) return
+
+    gsap.registerPlugin(ScrollTrigger)
+
+    const splitTypes = document.querySelectorAll('.scroll-reveal')
+
+    splitTypes.forEach((char,i) => {
+        const text = new SplitType(char, { types: 'words, chars' })
+
+        gsap.from(text.chars, {
+            scrollTrigger: {
+                trigger: char,
+                start: 'top 80%',
+                end: 'top 30%',
+                scrub: true,
+                markers: false
+            },
+            opacity: 0.2,
+            stagger: 0.1
+        })
+    })
+})
+
 </script>
 
 <template>
     <div class="my-16">
         <ContentTitle id="about-me" title="About me" />
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-            <div class="md:col-span-2 text-lg flex flex-col gap-4">
-                <p>Hello! My name is <Highlight>Kiriam</Highlight>, but people often call me <Highlight>Kiri</Highlight>. I am a {{ age }}-year-old full stack developer based in Manresa, Catalonia.</p>
-                <p>I hold a degree in <Highlight>ICT Systems Engineering</Highlight> from the Polytechnic University of Catalonia (UPC).</p>
-                <p>I'm all about crafting high-quality, well-designed websites—so much so that I haven't been replaced by A.I. yet.</p>
-                <p>In my downtime, I enjoy rock climbing. It's a great way to relax and de-stress, providing a refreshing break from everyday routines.</p>
-                <p>I have a strong affection for cats and share my home with two of them, along with one dog. Even though they sometimes make my life a bit <Highlight>chaotic</Highlight>, I wouldn't have it any other way.</p>
+            <div class="md:col-span-2 text-lg">
+                <p class="scroll-reveal">
+                    Hello! My name is <Highlight>Kiriam</Highlight>, but people often call me <Highlight>Kiri</Highlight>. I am a {{ age }}-year-old full stack developer based in Manresa, Catalonia.
+                    <br><br>
+                    I hold a degree in <Highlight>ICT Systems Engineering</Highlight> from the Polytechnic University of Catalonia (UPC).
+                    <br><br>
+                    I'm all about crafting high-quality, well-designed websites—so much so that I haven't been replaced by A.I. yet.
+                    <br><br>
+                    In my downtime, I enjoy rock climbing. It's a great way to relax and de-stress, providing a refreshing break from everyday routines.
+                    <br><br>
+                    I have a strong affection for cats and share my home with two of them, along with one dog. Even though they sometimes make my life a bit <Highlight>chaotic</Highlight>, I wouldn't have it any other way.
+                </p>
             </div>
             <img :src="portfolio" alt="Portfolio Picture" class="shadow-lg rounded-md h-full object-cover max-h-96 sm:max-h-none mx-auto">
         </div>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,46 @@
+<script setup>
+import Lenis from 'lenis'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+    const lenis = new Lenis()
+
+    lenis.on('scroll', (e) => {})
+
+    function raf(time) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+})
+
+</script>
+
 <template>
     <main class="relative min-h-svh antialiased">
         <slot />
     </main>
 </template>
+
+<style>
+html.lenis, html.lenis body {
+  height: auto;
+}
+
+.lenis.lenis-smooth {
+  scroll-behavior: auto !important;
+}
+
+.lenis.lenis-smooth [data-lenis-prevent] {
+  overscroll-behavior: contain;
+}
+
+.lenis.lenis-stopped {
+  overflow: hidden;
+}
+
+.lenis.lenis-smooth iframe {
+  pointer-events: none;
+}
+</style>
