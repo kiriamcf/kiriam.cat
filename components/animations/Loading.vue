5cd3a5f62fbd5f174cd41b3a2036d01e56fd1c49
kiriamcf <kiriamcf@gmail.com>
2024-10-13 17:04:35 +0200

    improvement: changed loading font sizes

--- components/animations/Loading.vue
+++ components/animations/Loading.vue
@@ -20,10 +20,10 @@ onMounted(() => {
 
     gsap.to('#percentage', { delay: 1, opacity: 0, duration: 1, ease: 'power4.out' });
 
-    animation.to('.hide', { y: -75, duration: 0.75, ease: 'power4.out', stagger: 0.02 });
-
     const fontSize = parseFloat(window.getComputedStyle(textElement.value).fontSize) * 0.8
     const offsets = [-fontSize, 0, fontSize, 2*fontSize];
+
+    animation.to('.hide', { y: -(fontSize+80), duration: 0.75, ease: 'power4.out', stagger: 0.02 });
     
     animation.to('.show', { 
         x: (index, target) => {
@@ -52,7 +52,7 @@ onMounted(() => {
 <template>
     <div id="overlay" class="fixed bg-white inset-0 z-10 font-lato overflow-hidden">
         <div class="relative w-full h-full flex justify-center items-center">
-            <p ref="textElement" class="inline-flex whitespace-pre text-2xl sm:text-6xl font-medium overflow-hidden">
+            <p ref="textElement" class="inline-flex whitespace-pre text-[7vw] sm:text-[5vw] font-medium overflow-hidden">
                 <span class="show">K</span>
                 <span class="hide">i</span>
                 <span class="hide">r</span>
@@ -78,7 +78,7 @@ onMounted(() => {
                 <span class="hide">s</span>
                 <span class="show">.</span>   
             </p>
-            <div id="percentage" class="absolute bottom-4 right-4 text-2xl sm:text-3xl">
+            <div id="percentage" class="absolute bottom-4 right-4 text-4xl">
                 <span>{{ tweened.number.toFixed() }}</span>
                 <span>%</span>
             </div>
